24d012078143d079c44c2f59b60a39c370f69606
MattW <drflamerock@yahoo.co.uk>
2020-03-08 23:51:22 +0000

    target Number inputs + var creation + var maths

--- Pizza Divider.js
+++ Pizza Divider.js
@@ -1,4 +1,11 @@
 const submitButton = document.getElementById('submit');
 submitButton.addEventListener('click, function(event) {
+let peopleNumber document.getElementById('people').value;
+let pizzaNumber document.getElementById('pizzas').value;
+let sliceNumber document.getElementById('slices').value;
+let pizzaSlices = sliceNumber * pizzaNumber;
+let fullTotal = pizzaSlices / peopleNumber;
+let sharedSlices = math.floor(fullTotal);
+let reminderSlices = fullTotal - sharedSlices * peopleNumber;
 
 }')
