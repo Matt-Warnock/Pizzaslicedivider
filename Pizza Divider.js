c5f24dcef634f7e956315e21c89063d67a8314d5
MattW <drflamerock@yahoo.co.uk>
2020-03-09 22:45:01 +0000

    add result and target var with innerHTML change answer class to id

--- Pizza Divider.js
+++ Pizza Divider.js
@@ -7,5 +7,7 @@ let pizzaSlices = sliceNumber * pizzaNumber;
 let fullTotal = pizzaSlices / peopleNumber;
 let sharedSlices = math.floor(fullTotal);
 let reminderSlices = fullTotal - sharedSlices * peopleNumber;
-
+let result = `You have ${sharedSlices} each and ${reminderSlices} left to fight over!`;
+const target = document.getElementById('answer');
+target.innerHTML = result;
 }')
