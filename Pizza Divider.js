befcfa740946175c30b0a0561876363df4264d35
MattW <drflamerock@yahoo.co.uk>
2020-03-10 11:40:32 +0000

    Fix js syntax errors, add *= method, replace form with div

--- Pizza Divider.js
+++ Pizza Divider.js
@@ -1,4 +1,5 @@
 const submitButton = document.getElementById('submit');
+<<<<<<< HEAD
 submitButton.addEventListener('click, function(event) {
 let peopleNumber document.getElementById('people').value;
 let pizzaNumber document.getElementById('pizzas').value;
@@ -11,3 +12,18 @@ let result = `You have ${sharedSlices} each and ${reminderSlices} left to fight 
 const target = document.getElementById('answer');
 target.innerHTML = result;
 }')
+=======
+submitButton.addEventListener('click', function(event) {
+let peopleNumber = document.getElementById('people').value;
+let pizzaNumber = document.getElementById('pizzas').value;
+let sliceNumber = document.getElementById('slices').value;
+let pizzaSlices = sliceNumber * pizzaNumber;
+let fullTotal = pizzaSlices / peopleNumber;
+let sharedSlices = Math.floor(fullTotal);
+let reminderSlices = fullTotal - sharedSlices;
+Math.round(reminderSlices *= peopleNumber);
+let result = `You have ${sharedSlices} each and ${reminderSlices} left to fight over!`;
+const target = document.getElementById('answer');
+target.innerHTML = result;
+})
+>>>>>>> 2d5e68c... rebase this commit
