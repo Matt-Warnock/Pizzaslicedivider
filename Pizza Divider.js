904d55f790f4d805490cfece96985c8dc5d2f3ad
MattW <drflamerock@yahoo.co.uk>
2020-03-10 12:23:50 +0000

    Remove all the crap it just added on the code

--- Pizza Divider.js
+++ Pizza Divider.js
@@ -1,18 +1,4 @@
 const submitButton = document.getElementById('submit');
-<<<<<<< HEAD
-submitButton.addEventListener('click, function(event) {
-let peopleNumber document.getElementById('people').value;
-let pizzaNumber document.getElementById('pizzas').value;
-let sliceNumber document.getElementById('slices').value;
-let pizzaSlices = sliceNumber * pizzaNumber;
-let fullTotal = pizzaSlices / peopleNumber;
-let sharedSlices = math.floor(fullTotal);
-let reminderSlices = fullTotal - sharedSlices * peopleNumber;
-let result = `You have ${sharedSlices} each and ${reminderSlices} left to fight over!`;
-const target = document.getElementById('answer');
-target.innerHTML = result;
-}')
-=======
 submitButton.addEventListener('click', function(event) {
 let peopleNumber = document.getElementById('people').value;
 let pizzaNumber = document.getElementById('pizzas').value;
@@ -26,4 +12,3 @@ let result = `You have ${sharedSlices} each and ${reminderSlices} left to fight 
 const target = document.getElementById('answer');
 target.innerHTML = result;
 })
->>>>>>> 2d5e68c... rebase this commit
