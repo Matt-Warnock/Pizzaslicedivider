64c12282a607e7047b157cebbadeb91184ca44f4
MattW <drflamerock@yahoo.co.uk>
2020-03-15 14:05:50 +0000

    Apply indents js html, add .jshintrc file

--- Pizza Divider.js
+++ Pizza Divider.js
@@ -1,14 +1,14 @@
 const submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', function(event) {
-let peopleNumber = document.getElementById('people').value;
-let pizzaNumber = document.getElementById('pizzas').value;
-let sliceNumber = document.getElementById('slices').value;
-let pizzaSlices = sliceNumber * pizzaNumber;
-let fullTotal = pizzaSlices / peopleNumber;
-let sharedSlices = Math.floor(fullTotal);
-let reminderSlices = fullTotal - sharedSlices;
-Math.round(reminderSlices *= peopleNumber);
-let result = `You have ${sharedSlices} each and ${reminderSlices} left to fight over!`;
-const target = document.getElementById('answer');
-target.innerHTML = result;
-})
+  let peopleNumber = document.getElementById('people').value;
+  let pizzaNumber = document.getElementById('pizzas').value;
+  let sliceNumber = document.getElementById('slices').value;
+  let pizzaSlices = sliceNumber * pizzaNumber;
+  let fullTotal = pizzaSlices / peopleNumber;
+  let sharedSlices = Math.floor(fullTotal);
+  let reminderSlices = fullTotal - sharedSlices;
+  Math.round(reminderSlices *= peopleNumber);
+  let result = `You have ${sharedSlices} each and ${reminderSlices} left to fight over!`;
+  const target = document.getElementById('answer');
+  target.innerHTML = result;
+});
