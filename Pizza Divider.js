42c576a2f20bc9b3dc23131479ec61704a42b2c6
MattW <drflamerock@yahoo.co.uk>
2020-03-17 20:44:51 +0000

    Space js for better reading fix js remider slice problem

--- Pizza Divider.js
+++ Pizza Divider.js
@@ -1,13 +1,17 @@
 const submitButton = document.getElementById('submit');
+
 submitButton.addEventListener('click', function(event) {
+
   let peopleNumber = document.getElementById('people').value;
   let pizzaNumber = document.getElementById('pizzas').value;
   let sliceNumber = document.getElementById('slices').value;
+
   let pizzaSlices = sliceNumber * pizzaNumber;
   let fullTotal = pizzaSlices / peopleNumber;
   let sharedSlices = Math.floor(fullTotal);
-  let reminderSlices = fullTotal - sharedSlices;
-  Math.round(reminderSlices *= peopleNumber);
+  let sharedTotal = sharedSlices * peopleNumber;
+  let reminderSlices = pizzaSlices - sharedTotal;
+
   let result = `You have ${sharedSlices} each and ${reminderSlices} left to fight over!`;
   const target = document.getElementById('answer');
   target.innerHTML = result;
