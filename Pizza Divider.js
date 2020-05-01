1faa74c78280b674aea2fc54a43700134e092e40
MattW <drflamerock@yahoo.co.uk>
2020-05-01 13:36:48 +0100

    Add pizza Dividing logic object method

--- Pizza Divider.js
+++ Pizza Divider.js
@@ -1,18 +1,57 @@
-const submitButton = document.getElementById('submit');
+//const submitButton = document.getElementById('submit');
 
-submitButton.addEventListener('click', function(event) {
+//submitButton.addEventListener('click', function(event) {
 
-  let peopleNumber = document.getElementById('people').value;
-  let pizzaNumber = document.getElementById('pizzas').value;
-  let sliceNumber = document.getElementById('slices').value;
+//  let peopleNumber = document.getElementById('people').value;
+//  let pizzaNumber = document.getElementById('pizzas').value;
+//  let sliceNumber = document.getElementById('slices').value;
 
-  let pizzaSlices = sliceNumber * pizzaNumber;
-  let fullTotal = pizzaSlices / peopleNumber;
-  let sharedSlices = Math.floor(fullTotal);
-  let sharedTotal = sharedSlices * peopleNumber;
-  let reminderSlices = pizzaSlices - sharedTotal;
+//  let pizzaSlices = sliceNumber * pizzaNumber;
+//  let fullTotal = pizzaSlices / peopleNumber;
+//  let sharedSlices = Math.floor(fullTotal);
+//  let sharedTotal = sharedSlices * peopleNumber;
+//  let reminderSlices = pizzaSlices - sharedTotal;
 
-  let result = `You have ${sharedSlices} each and ${reminderSlices} left to fight over!`;
-  const target = document.getElementById('answer');
-  target.innerHTML = result;
-});
+//  let result = `You have ${sharedSlices} each and ${reminderSlices} left to fight over!`;
+//  const target = document.getElementById('answer');
+//  target.innerHTML = result;
+//});
+
+
+(function () {
+
+let userInterface = {
+  submitButton: document.getElementById('submit'),
+  people: document.getElementById('people'),
+  pizza: document.getElementById('pizzas'),
+  slice: document.getElementById('slices'),
+  displayTarget: document.getElementById('answer'),
+  clickStart () {
+    this.submitButton.addEventListener('click', event => {
+
+      pizzaDivider.divideSlices(this.people.value, this.pizza.value, this.slice.value);
+      this.displayResults(pizzaDivider.sharedSlices, pizzaDivider.reminderSlices);
+      
+    });
+  },
+  displayResults (sliceEach, sliceLeft) {
+    this.displayTarget.textContent = `You have ${sliceEach} each and ${sliceLeft} left to fight over!`;
+  }
+};
+
+
+let pizzaDivider = {
+    sharedSlices: 0,
+    reminderSlices: 0,
+    divideSlices (peopleNumber, pizzaNumber, sliceNumber) {
+      let pizzaSlices = sliceNumber * pizzaNumber,
+      fullTotal = pizzaSlices / peopleNumber;
+      this.sharedSlices = Math.floor(fullTotal);
+      let sharedTotal = this.sharedSlices * peopleNumber;
+      this.reminderSlices = pizzaSlices - sharedTotal;
+    },
+  };
+
+userInterface.clickStart();
+
+})();
